13b460dcbb817f9b7e31209d7680389307850a29
famedina1 <114297143+famedina1@users.noreply.github.com>
2023-05-25 22:20:51 -0500

    Update Component4.js

--- src/components/Component4.js
+++ src/components/Component4.js
@@ -1,10 +1,21 @@
+
+ 
+const divStyle = {
+  width: '18rem'
+};
+
 function Component4() {
     return (
       <div>
-        <h2>Componente 4</h2>
+        <div className="card" style={divStyle}>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>           
+          </div>
+        </div>
       </div>
     );
   }
   
   export default Component4;
-  
