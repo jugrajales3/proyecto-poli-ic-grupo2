1d63f81ede9ea3964c74b630f60225d752733569
Manuel Agudo <89302854+Azura01@users.noreply.github.com>
2023-05-27 19:15:25 -0500

    Agregando descripción de Docker en el componente

--- src/components/Component3.js
+++ src/components/Component3.js
@@ -1,7 +1,13 @@
 function Component3() {
     return (
       <div>
-        <h2>Componente 3</h2>
+        <div className="card" style={divStyle}>
+          <div className="card-body">
+            <h5 className="card-title">Docker</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Tecnología de organización en contenedores</h6>
+            <p className="card-text">Docker es una plataforma de código abierto que permite a los desarrolladores crear, desplegar, ejecutar y gestionar contenedores, que son componentes estandarizados y ejecutables que combinan el código fuente de aplicación con las dependencias y las bibliotecas del sistema operativo (SO) necesarias para ejecutar dicho código en cualquier entorno.</p>           
+          </div>
+        </div>
       </div>
     );
   }
