bf4f5d19b36139e39462ff12bbd6037e4871ad8c
Carlos Andrés Sánchez <57685118+dev2619@users.noreply.github.com>
2023-05-27 19:48:37 -0500

    Update Component2.js

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -9,10 +9,18 @@ function Component2() {
 				style={divStyle}>
 				<div className='card-body'>
 					<h5 className='card-title'>Jenkins</h5>
-					<h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
+					<h6 className='card-subtitle mb-2 text-muted'>
+						Herramienta De Automatización De Código Abierto
+					</h6>
 					<p className='card-text'>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
+						Esta herramienta es ampliamente utilizada en el desarrollo de
+						software, proporciona un entorno de integración continua que permite
+						a los equipos de desarrollo automatizar tareas como la compilación,
+						las pruebas y la implementación de aplicaciones. Jenkins se utiliza
+						para agilizar el proceso de desarrollo y mejorar la eficiencia
+						mediante la automatización de tareas repetitivas, lo que ayuda a los
+						equipos a entregar software de alta calidad de manera más rápida y
+						confiable.
 					</p>
 				</div>
 			</div>
